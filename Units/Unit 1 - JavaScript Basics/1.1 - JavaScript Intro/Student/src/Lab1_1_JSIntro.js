03c94ca1fc9e77c37e3b0b79d17a591febd458cf
Mclovin-B <Cbarton28034@Columbushs.com>
2025-10-23 09:08:02 -0400

    i finished 1.1

--- Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
+++ Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
@@ -4,18 +4,33 @@
    =========================================================== */
 
 // TODO 1: Print your full name with console.log()
-
+console.log(Christopher Barton);
 
 // TODO 2: Declare variable favoriteFood, assign "Steak", then reassign "Pizza"
-
+let favoriteFood = "Steak"
+favoriteFood = "Pizza"
 
 // TODO 3: Create variable mystery, assign a string, then a number, print type each time
+let mystery = "burger";
+console.log(typeof mystery);
 
+mystery = 45;
+console.log(typeof mystery);
 
 // TODO 4: Declare const schoolName, try to reassign
+const schoolName = "Christopher Columbus High School ";
+console.log(schoolName);
 
+schoolName = "New School Name"; 
 
 // TODO 5: Create three valid camelCase variables and print them
+let firstName = "Christopher";
+let lastLoginDate = "2025-10-20";
+let numberOfVisits = 5;
+
+console.log(firstName);
+console.log(lastLoginDate);
+console.log(numberOfVisits);
 
 
 // 🚀 Stretch Goals (optional)
@@ -25,7 +40,12 @@
 /* ===========================================================
    🎟 EXIT TICKET
    1. What new concept or skill did you learn in this lab?
+how to declare a variable and reassign it
+
    2. How did you apply logic or conditionals in your code?
+
    3. What challenges did you encounter, and how did you solve them?
+trying not to make a lot of mistakes was the biggest challenge for me especially spelling
+
    4. Where else could this programming concept be useful?
    =========================================================== */
