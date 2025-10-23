33c2aec64d08635246ed6ece0d2d97cc6a15cf8f
Mclovin-B <Cbarton28034@Columbushs.com>
2025-10-23 09:45:49 -0400

    final edit of 1.1 finished

--- Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
+++ Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
@@ -12,9 +12,12 @@ favoriteFood = "Pizza"
 
 // TODO 3: Create variable mystery, assign a string, then a number, print type each time
 let mystery = "burger";
-console.log(typeof mystery);
+console.log(typeof mystery)
+
 
 mystery = 45;
+
+
 console.log(typeof mystery);
 
 // TODO 4: Declare const schoolName, try to reassign
@@ -26,7 +29,7 @@ schoolName = "New School Name";
 // TODO 5: Create three valid camelCase variables and print them
 let firstName = "Christopher";
 let lastLoginDate = "2025-10-20";
-let numberOfVisits = 5;
+let numberOfVisits = 600000000;
 
 console.log(firstName);
 console.log(lastLoginDate);
@@ -36,16 +39,20 @@ console.log(numberOfVisits);
 // 🚀 Stretch Goals (optional)
 // - Create a variable age and print "I am X years old" using concatenation
 // - Repeat using template literals
-
+let age = 15;
+console.log("I am " + age + " years old");
+console.log(`I am ${age} years old`);
 /* ===========================================================
    🎟 EXIT TICKET
    1. What new concept or skill did you learn in this lab?
-how to declare a variable and reassign it
+how to declare a variable and reassign it and use let instead of var 
 
    2. How did you apply logic or conditionals in your code?
 
    3. What challenges did you encounter, and how did you solve them?
 trying not to make a lot of mistakes was the biggest challenge for me especially spelling
+also not knowing what a camelcase was 
 
    4. Where else could this programming concept be useful?
+   Organizing data and info 
    =========================================================== */
