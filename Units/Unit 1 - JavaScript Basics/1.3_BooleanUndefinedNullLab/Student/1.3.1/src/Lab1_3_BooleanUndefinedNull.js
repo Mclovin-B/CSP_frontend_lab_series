28e6720668607973bbdb006a93de417894097e57
Mclovin-B <Cbarton28034@Columbushs.com>
2025-11-04 09:30:05 -0500

    Commit burger

--- Units/Unit 1 - JavaScript Basics/1.3_BooleanUndefinedNullLab/Student/1.3.1/src/Lab1_3_BooleanUndefinedNull.js
+++ Units/Unit 1 - JavaScript Basics/1.3_BooleanUndefinedNullLab/Student/1.3.1/src/Lab1_3_BooleanUndefinedNull.js
@@ -4,20 +4,37 @@
 
 // TODO 1: Booleans
 // - Create isStudent = true, hasLicense = false
+node src/Lab1_3_BooleanUndefinedNull.js
 // - Print results of true || false, true && false, !true
+isStudent = true;
+hasLicense = false;
 
+true  || false;
+true  && false;
+!true;
 // TODO 2: Short-circuit evaluation
-// - Print result of false && (5 > 2)
-// - Print result of true || (2 > 5)
+ false && (5 > 2);
+
+ true    || (2 > 5);
+
+ let grade; = 77;
 
 // TODO 3: Undefined
 // - Declare variable grade and print it
+let grade; 
 
 // TODO 4: Null
+let city = null;
+city = "miami";
+print(city);
 // - Create city = null, print it
 // - Reassign city = "Miami" and print again
 
 // TODO 5: Immutability
+let word = "hello";
+word[0] = "H";
+
+word = "hello";
 // - Create word = "hello"
 // - Try word[0] = "H", print word
 // - Reassign word = "Hello" and print again
@@ -25,3 +42,7 @@
 // 🚀 Stretch Goals
 // - Check if number is between 10 and 20 using &&
 // - Check if city is "Miami" or "Berlin" using ||
+  
+what is the difference between null and undefined in javascript
+when you put null its like a placeholder for no value 
+undefined is something that doesnt have anassigned value yet s
