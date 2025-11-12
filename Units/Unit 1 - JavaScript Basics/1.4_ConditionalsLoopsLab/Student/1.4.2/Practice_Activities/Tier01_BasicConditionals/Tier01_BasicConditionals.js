e77031ca4cb4b251830287c163c851098866385c
Mclovin-B <Cbarton28034@Columbushs.com>
2025-11-12 10:09:38 -0500

    commit possibly

--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier01_BasicConditionals/Tier01_BasicConditionals.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier01_BasicConditionals/Tier01_BasicConditionals.js
@@ -1,2 +1,30 @@
-// Your code goes here. Follow the steps in README.md and run from index.html.
-console.log("TEST - TIER 1")
+.html.
+
+     console.log("TEST - TIER 1")
+
+        let grade = prompt("what is your grade ?");
+  
+        switch(grade) {
+        case nightime :
+            console.log("A");
+
+        break;
+
+        case value2:
+        console.log("B");
+
+        break;
+
+        case nightime :
+        console.log("C");
+        break;
+        case value2
+        console.log("D");
+        default:
+            break;
+        }
+
+let age = Number(prompt("what is your age ?"));
+
+        let (age >=) 18
+        console.log("you cannot vote");
